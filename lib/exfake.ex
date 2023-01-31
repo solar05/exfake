9d78b6fd73ea4625f681121783faa55610220cbd
solar05 <artem00298@gmail.com>
2023-01-31 03:39:53 +0300

    Add names and phones

--- lib/exfake.ex
+++ lib/exfake.ex
@@ -3,16 +3,39 @@ defmodule Exfake do
   Documentation for `Exfake`.
   """
 
+  alias Datasets.{Names, Phones}
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exfake.hello()
-      :world
-
+  Generates first name.
   """
-  def hello do
-    :world
+  @spec first_name :: String.t()
+  def first_name do
+    Enum.random(Names.first_names())
+  end
+
+  @doc """
+  Generates last name.
+  """
+  @spec last_name :: String.t()
+  def last_name do
+    Enum.random(Names.last_names())
+  end
+
+  @doc """
+  Generates first name + last name combination.
+  """
+  @spec person :: String.t()
+  def person do
+    "#{first_name()} #{last_name()}"
+  end
+
+  @doc """
+  Generates phone number which formatted randomly.
+  """
+  @spec phone_number :: String.t()
+  def phone_number do
+    Phones.formats()
+    |> Enum.random()
+    |> String.replace("#", fn _ -> Integer.to_string(Enum.random(0..9)) end)
   end
 end
--- test/exfake_test.exs
+++ test/exfake_test.exs
@@ -1,8 +1,4 @@
 defmodule ExfakeTest do
   use ExUnit.Case
   doctest Exfake
-
-  test "greets the world" do
-    assert Exfake.hello() == :world
-  end
 end
